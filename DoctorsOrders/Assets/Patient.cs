34bc3cc8fd78604224625b10e321fdb77fb251d8
declanqc <C20465442@mytudublin.ie>
2022-10-23 15:06:06 +0100

    added demo doctor scene with moving characters

--- DoctorsOrders/Assets/Patient.cs
+++ DoctorsOrders/Assets/Patient.cs
@@ -10,15 +10,58 @@ public class Patient : MonoBehaviour
     public float maxHorizontal = 600;
     public float minHorizontal = -600;
 
+    public Sprite Patient1;
+    public Sprite Patient2;
+    public Sprite Patient3;
+    public Sprite Patient4;
+    public Sprite Patient5;
+
+    public int imgNumberCount;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
+        GetComponent<Image>().sprite = Patient1;
         rectTransform = GetComponent<RectTransform>();
+        Vector2 position = rectTransform.anchoredPosition;
+
+        position.x = minHorizontal;
     }
 
-    // Update is called once per frame
+
+    void ImageSwap()
+    {
+        switch (imgNumberCount)
+        {
+            case 0:
+                GetComponent<Image>().sprite = Patient2;
+                imgNumberCount++;
+                break;
+            case 1:
+                GetComponent<Image>().sprite = Patient3;
+                imgNumberCount++;
+                break;
+            case 2:
+                GetComponent<Image>().sprite = Patient4;
+                imgNumberCount++;
+                break;
+            case 3:
+                GetComponent<Image>().sprite = Patient5;
+                imgNumberCount++;
+                break;
+                
+        }
+
+
+
+      
+      
+    }
     void Update()
     {
+
         Vector2 position = rectTransform.anchoredPosition;
 
         position.x += speed * Time.deltaTime;
@@ -26,6 +69,10 @@ public class Patient : MonoBehaviour
         if (position.x > maxHorizontal)
         {
             position.x = minHorizontal;
+
+            ImageSwap();
+            
+
         }
 
         rectTransform.anchoredPosition = position;
